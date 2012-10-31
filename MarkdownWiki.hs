bf186fd69a891ff126ff9fa55e22646f834df792
Rickard Lindberg <ricli85@gmail.com>
2012-10-31 10:31:05 +0100

    expand $ROOT in links

--- MarkdownWiki.hs
+++ MarkdownWiki.hs
@@ -24,7 +24,7 @@ writePages :: FilePath -> Pages -> IO ()
 writePages dir pages = mapM_ writePage (M.assocs pages)
     where
         writePage (name, pandoc) = do
-            let html = writeHtmlString defaultWriterOptions (process pandoc)
+            let html = writeHtmlString defaultWriterOptions (process (getRoot "") pandoc)
             let templified = templify name html pages
             writeFile (dir </> name ++ ".html") templified
 
--- Process.hs
+++ Process.hs
@@ -1,15 +1,21 @@
 module Process where
 
+import Data.String.Utils
 import Text.Pandoc
 import WikiName
 
-process :: Pandoc -> Pandoc
-process = bottomUp linkify
+process :: String -> Pandoc -> Pandoc
+process root = bottomUp (expandRoot root) . bottomUp linkify
 
 linkify :: [Inline] -> [Inline]
 linkify []           = []
 linkify (Str str:xs) = toLink str ++ linkify xs
 linkify (x:xs)       = x           : linkify xs
+
+expandRoot :: String -> Inline -> Inline
+expandRoot root (Link  x (url, y)) = Link  x (replace "$ROOT" root url, y)
+expandRoot root (Image x (url, y)) = Image x (replace "$ROOT" root url, y)
+expandRoot root x                  = x
 
 toLink :: String -> [Inline]
 toLink = map toInline . splitOnWikiNames
