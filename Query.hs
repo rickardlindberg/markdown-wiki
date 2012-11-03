ed5d9247cfd038582f2d33717c176a6c5c300f04
Rickard Lindberg <ricli85@gmail.com>
2012-11-03 10:04:26 +0100

    call extractWikiLinks only once per graphFrom call

--- Query.hs
+++ Query.hs
@@ -6,6 +6,11 @@ import Text.Pandoc
 import Types
 import WikiName
 
+type LinkWeb = M.Map WikiName [WikiName]
+
+linkWebFrom :: Pages -> LinkWeb
+linkWebFrom = M.map extractWikiLinks
+
 data Graph = Graph
     { vertices :: [(Int, WikiName)]
     , edges    :: [(WikiName, WikiName)]
@@ -13,49 +18,45 @@ data Graph = Graph
 
 graphFrom :: WikiName -> Pages -> Graph
 graphFrom name pages =
-    let vertices = expandTree name pages
-        edges    = findLinks (map snd vertices) pages
+    let linkWeb  = linkWebFrom pages
+        vertices = expandTree name linkWeb
+        edges    = findLinks (map snd vertices) linkWeb
     in Graph vertices edges
 
-expandTree :: WikiName -> Pages -> [(Int, WikiName)]
-expandTree name pages =
+expandTree :: WikiName -> LinkWeb -> [(Int, WikiName)]
+expandTree name linkWeb =
     let level1 = [name]
-        level2 = filter (\x -> x `notElem` level1) $ nextLevel level1 pages
-        level3 = filter (\x -> x `notElem` level1 && x `notElem` level2) $ nextLevel level2 pages
+        level2 = filter (\x -> x `notElem` level1) $ nextLevel level1 linkWeb
+        level3 = filter (\x -> x `notElem` level1 && x `notElem` level2) $ nextLevel level2 linkWeb
         l1     = map (\x -> (1, x)) level1
         l2     = map (\x -> (2, x)) level2
         l3     = map (\x -> (3, x)) level3
     in l1 ++ l2 ++ l3
 
-findLinks :: [WikiName] -> Pages -> [(WikiName, WikiName)]
-findLinks names pages = concatMap findLinksFrom names
+findLinks :: [WikiName] -> LinkWeb -> [(WikiName, WikiName)]
+findLinks names linkWeb = concatMap findLinksFrom names
     where
         findLinksFrom from = map (\to -> (from, to)) (linkedPages from)
-        linkedPages name = filter (`elem` names) (findPagesLinkedFrom name pages)
+        linkedPages from = filter (`elem` names) (M.findWithDefault [] from linkWeb)
 
-nextLevel :: [WikiName] -> Pages -> [WikiName]
-nextLevel names pages =
-    let to   = concatMap (`findPagesLinkingTo` pages) names
-        from = concatMap (`findPagesLinkedFrom` pages) names
+nextLevel :: [WikiName] -> LinkWeb -> [WikiName]
+nextLevel names linkWeb =
+    let to   = concatMap (`findPagesLinkingTo` linkWeb) names
+        from = concatMap (`findPagesLinkedFrom` linkWeb) names
     in uniqList $ to ++ from
-    where
-        uniqList :: Ord a => [a] -> [a]
-        uniqList = S.toList . S.fromList
 
-findPagesLinkingTo :: WikiName -> Pages -> [WikiName]
-findPagesLinkingTo name pages = M.keys $ M.filter (hasLinkTo name) pages
-    where
-        hasLinkTo name page = name `elem` extractWikiLinks page
+findPagesLinkingTo :: WikiName -> LinkWeb -> [WikiName]
+findPagesLinkingTo name linkWeb = M.keys $ M.filter (name `elem`) linkWeb
 
-findPagesLinkedFrom :: WikiName -> Pages -> [WikiName]
-findPagesLinkedFrom name pages =
-    case M.lookup name pages of
-        Nothing   -> []
-        Just page -> extractWikiLinks page
+findPagesLinkedFrom :: WikiName -> LinkWeb -> [WikiName]
+findPagesLinkedFrom name linkWeb = M.findWithDefault [] name linkWeb
 
 extractWikiLinks :: Pandoc -> [WikiName]
-extractWikiLinks = queryWith findWikiLinks
+extractWikiLinks = uniqList . queryWith findWikiLinks
     where
         findWikiLinks :: Inline -> [WikiName]
         findWikiLinks (Str str) = map snd $ filter fst $ splitOnWikiNames str
         findWikiLinks _         = []
+
+uniqList :: Ord a => [a] -> [a]
+uniqList = S.toList . S.fromList
